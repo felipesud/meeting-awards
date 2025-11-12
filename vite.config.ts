eff83b4b6dc3cb91b45076d4bb08e0ec45f8a9d9
Felipe <felipe.belisario@ericsson.com>
2025-11-12 15:35:41 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 const NOME_DO_SEU_REPO = 'meeting-awards'; 
 
 export default defineConfig({
     base: `/${NOME_DO_SEU_REPO}/`, 
+    
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                
+                admin: resolve(__dirname, 'admin.html')
+            }
+        }
+    }
 });
